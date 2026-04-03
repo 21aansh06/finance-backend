6fc2d2fb012b60917d0eec4520e3ebae3ffdf9ab
Aansh Malhotra <aanshmalhotra49@gmail.com>
2026-04-04 02:01:52 +0530

    constructor error in AppError

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -2,6 +2,8 @@ export class AppError extends Error {
   constructor(message: string, public statusCode: number = 400) {
     super(message);
     this.name = this.constructor.name;
-    Error.captureStackTrace(this, this.constructor);
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor);
+    }
   }
 }
